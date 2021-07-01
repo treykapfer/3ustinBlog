4da97b63ac2f3ab8c2297dbfbb6afcf18207e864
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-07-01 14:13:19 -0500

    add post repo

--- src/main/java/com/treykapfer/ustinblog/repositories/PostRepository.java
+++ src/main/java/com/treykapfer/ustinblog/repositories/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.treykapfer.ustinblog.models.Post;
@@ -12,4 +13,10 @@ import com.treykapfer.ustinblog.models.Post;
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
     List<Post> findAll();
+
+    @Query("SELECT * FROM posts ORDER BY created_at DESC;")
+    List<Post> findAllReverse();
+
+
+
 }
